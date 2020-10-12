426bc537e4d9dc27aad762dd127a86cbc86041c8
ABEGalindo <abraham@siclo.com>
2020-10-12 16:49:22 -0500

    AddValidations

--- SICLOPLUSMOBILEANDROID/src/test/java/test/SCL_000001.java
+++ SICLOPLUSMOBILEANDROID/src/test/java/test/SCL_000001.java
@@ -3,11 +3,29 @@
  */
 package test;
 
+import java.io.IOException;
+
+import org.junit.Test;
+
 /**
  *@author Abraham Galindo
  *@date 12 oct 2020
- *@description 
+ *@description Esta clase contiene metodos que hacen llamado a los modulos para completar los pasos de los TC
  */
-public class SCL_000001 {
-
+public class SCL_000001  extends BaseTest {
+	@Test
+	public void login() throws IOException {
+		try {
+			int step = 0;
+			System.out.println("Step:" + step + "... Iniciando la aplicación");
+			//driver.get("http://google.com");
+		    //Assert.assertEquals("Title should be Google", "Google", driver.getTitle());
+			
+			
+		}catch(Exception e){
+			System.out.println("Fallo el script");
+		}
+		
+	}
 }
+
